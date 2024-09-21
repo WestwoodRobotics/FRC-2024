13f5052328b5b3913d145aa3af5b7968505103e0
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-09-21 14:51:17 +0000

    Update devcontainer.json and post_create.sh scripts

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -7,7 +7,7 @@ echo "Post-create script starting..."
 if ! command -v wget &> /dev/null
 then
     echo "wget could not be found, installing..."
-    sudo apt update && apt install -y wget
+    sudo sudo apt update && sudo apt install -y wget
 else
     sudo echo "wget is already installed"
 fi
@@ -16,7 +16,7 @@ fi
 if ! dpkg -l | grep -q openjdk-21-jre
 then
     echo "openjdk-21-jre could not be found, installing..."
-    sudo apt install -y openjdk-21-jre
+    sudo sudo apt update && sudo apt install -y openjdk-21-jre
 else
     sudo echo "openjdk-21-jre is already installed"
 fi
@@ -25,12 +25,16 @@ fi
 sudo wget "https://github.com/wpilibsuite/vscode-wpilib/releases/download/v2024.3.2/vscode-wpilib-2024.3.2.vsix"
 
 # Install the downloaded vsix file on VSCode
-sudo code --install-extension vscode-wpilib-2024.3.2.vsix
+code --install-extension vscode-wpilib-2024.3.2.vsix
 
 # Remove the downloaded vsix file after installation
 sudo rm vscode-wpilib-2024.3.2.vsix
 
 # Set ownership of /home/vscode directory to vscode user
 sudo chown -R vscode:vscode /home/vscode
+
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+source ~/.bashrc
 
 echo "Post-create script finished."
