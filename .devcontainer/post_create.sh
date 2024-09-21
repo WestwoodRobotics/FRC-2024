8c62b122fb7f17197ea5d52def280dee02980a55
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-09-20 22:01:21 -0500

    Add commands to download, install, and remove vscode-wpilib-2024.3.2.vsix file
    * Download the vscode-wpilib-2024.3.2.vsix file using `sudo wget`
    * Install the downloaded vsix file on VSCode using `code --install-extension`
    * Remove the downloaded vsix file after installation using `sudo rm`

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -21,37 +21,13 @@ else
     sudo echo "openjdk-21-jre is already installed"
 fi
 
-# Extract year and version from the URL
-URL="https://packages.wpilib.workers.dev/installer/v2024.3.2/Linux/WPILib_Linux-2024.3.2.tar.gz"
-VERSION=$(echo $URL | grep -oP 'v\K[0-9]{4}\.[0-9]+\.[0-9]+')
-YEAR=${VERSION%%.*}
+# Download the vscode-wpilib-2024.3.2.vsix file
+sudo wget "https://github.com/wpilibsuite/vscode-wpilib/releases/download/v2024.3.2/vscode-wpilib-2024.3.2.vsix"
 
-# Check if WPILib is already set up
-if [ ! -d "$HOME/wpilib/${VERSION%%.*}" ]; then
-    echo "Downloading WPILib..."
-    sudo wget $URL
-    echo "Extracting WPILib..."
-    sudo tar -xzvf WPILib_Linux-$VERSION.tar.gz
-    echo "Setting up WPILib directory..."
-    sudo mkdir ${VERSION%%.*}
-    cd ${VERSION%%.*}
-    sudo mv ../WPILib_Linux-$VERSION/WPILib_Linux-$VERSION-artifacts.tar.gz .
-    echo "Extracting WPILib artifacts..."
-    sudo tar xf WPILib_Linux-$VERSION-artifacts.tar.gz
-    sudo rm WPILib_Linux-$VERSION-artifacts.tar.gz
-    cd ..
-    sudo mkdir -p ~/wpilib
-    sudo mv ${VERSION%%.*} ~/wpilib
-    sudo rm -rf WPILib_Linux-*
-    # Run ToolsUpdater.py
-    cd ~/wpilib/$YEAR/tools/ && sudo python3 ToolsUpdater.py
+# Install the downloaded vsix file on VSCode
+sudo code --install-extension vscode-wpilib-2024.3.2.vsix
 
-    # Install VS Code extensions
-    cd ~/wpilib/$YEAR/vsCodeExtensions && sudo find . -name "*.vsix" | sudo xargs -I {} code --install-extension {}
-
-
-else
-    echo "WPILib is already set up"
-fi
+# Remove the downloaded vsix file after installation
+sudo rm vscode-wpilib-2024.3.2.vsix
 
 echo "Post-create script finished."
